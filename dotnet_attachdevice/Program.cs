3d88cdca23b08daa1f56ad0144304145ba1f618e
truepele <truepele@gmail.com>
2014-07-09 19:59:36 +0300

    3

--- dotnet_attachdevice/Program.cs
+++ dotnet_attachdevice/Program.cs
@@ -14,11 +14,65 @@ namespace dotnet_attachdevice
         {
             ApiWrapper api = new ApiWrapper();
 
+            try
+            {
+
+            
             if (api.Init())
             {
-                
+                Console.WriteLine("Api initialized.");
+                var devices = api.GetAvailableDevices();
+                api.DeviceDataReceived += ApiOnDeviceDataReceived;
+                if (devices != null)
+                {
+                    Console.WriteLine("Found {0} devices", devices.Length);
+                    var firstdevice = devices.FirstOrDefault();
+                    
+                    if (firstdevice != null)
+                    {
+                        Console.WriteLine("Device found: {0}", firstdevice.szFriendlyName);
+
+                        var attachresult = api.AttachDevice(firstdevice);
+                        Console.WriteLine("attachresult: {0} ", attachresult);
+                        
+                    }
+                }
             }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.ToString());
+            }
+
+
+            Console.WriteLine("Press enter key...");
+            Console.ReadLine();
+            api.DeviceDataReceived -= ApiOnDeviceDataReceived;
+
+            try
+            {
+                api.DetachDevice();
+            }
+            finally
+            {
+                api.ReleaseSession();
+            }
+
         }
 
+        private static void ApiOnDeviceDataReceived(object userData, NvtlEventTypeManaged eventType, uint size, uint value)
+        {
+            switch (eventType)
+            {
+                case NvtlEventTypeManaged.NW_EVENT_DEVICE_ATTACHED:
+                    Console.WriteLine("Event received: {0} - {1}", "NW_EVENT_DEVICE_ATTACHED", value);
+                    break;
+                    //if (eventType == NvtlEventTypeManaged.NW_EVENT_DEVICE_ATTACHED) 
+                default:
+                    Console.WriteLine("Event received: {0} - {1}", eventType.ToString(), value);
+                    break;
+            }
+            
+        }
     }
 }
